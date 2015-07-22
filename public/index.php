ce736f216e98b0fdec06bf778f5293533c66b06b
sapanto <sapanto@gmail.com>
2015-07-22 22:08:27 +0200

    Primo commit da Zend Studio Mac - Ho creato un progetto partendo da un repo di GitHub (vuoto) Adesso l'ho riempito con dei files e lo sto committando

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  GITHUB token 09ed18fbf03b460aa367b6d77c16c24b7cf20f99
+ *  
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
